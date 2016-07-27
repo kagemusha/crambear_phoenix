73d5ebcfc14edcae9b42b9909de400cab15463a5
kagemusha <mmadrid99@gmail.com>
2016-07-26 22:16:18 -0400

    .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,15 @@
+import Ecto.Queryable
+import Ecto.Query, only: [from: 1, from: 2]
+alias CrambearPhoenix.Repo
+alias CrambearPhoenix.Cardset
+alias CrambearPhoenix.Card
+alias CrambearPhoenix.Tag
+
+defmodule R do
+  def reload! do
+    Mix.Task.reenable "compile.elixir"
+    Application.stop(Mix.Project.config[:app])
+    Mix.Task.run "compile.elixir"
+    Application.start(Mix.Project.config[:app], :permanent)
+  end
+end
