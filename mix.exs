4aceff5c80d324a6d2ad25ea09ef3e28a87d07b7
kagemusha <mmadrid99@gmail.com>
2016-10-19 20:06:08 -0400

    drop db when test

--- mix.exs
+++ mix.exs
@@ -58,6 +58,6 @@ defmodule CrambearPhoenix.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.drop", "ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
